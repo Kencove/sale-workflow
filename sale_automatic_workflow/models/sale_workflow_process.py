cc335de9690dc08b848ea3b02d9de9f4289d4218
Atchuthan, Sodexis <atchuthan@sodexis.com>
2021-03-16 16:08:40 +0530

    [IMP] Add additional functionalities

--- sale_automatic_workflow/models/sale_workflow_process.py
+++ sale_automatic_workflow/models/sale_workflow_process.py
@@ -32,6 +32,7 @@ class SaleWorkflowProcess(models.Model):
         return self.env["ir.filters"].browse()
 
     name = fields.Char()
+    default = fields.Boolean()
     picking_policy = fields.Selection(
         selection=[
             ("direct", "Deliver each product when available"),
@@ -134,3 +135,5 @@ class SaleWorkflowProcess(models.Model):
         string="Payment Filter Domain",
         related="payment_filter_id.domain",
     )
+    auto_commit = fields.Boolean()
+    search_limit = fields.Integer(default=20)
